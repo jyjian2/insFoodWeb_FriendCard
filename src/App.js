7b256300cbaf3850e46b12cccc5b6047362f5b73
Jia-Yi Jian <jyjian@illinois.edu>
2020-10-31 21:41:53 -0500

    Card back to normal

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Card from "./components/Card";
-import UserMainPage from './components/UserMainPage';
-
-
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-       <Card username="Jia-Yi Jian"/>
-      {/*<UserMainPage/>*/}
-      
-      
+	  <Card username="Jia-Yi Jian"/>
     </div>
-
   );
 }
 
